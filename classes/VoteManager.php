1228e27caea8429d96c8ccc3026b91147a1187bd
Mohamad Zangane <mohamad.zangane@ymail.com>
2025-05-07 13:48:06 +0330

    Create VoteManager.php

---
+++ classes/VoteManager.php
@@ -0,0 +1,27 @@
+<?php
+
+class VoteManager {
+    private $poll;
+
+    public function __construct(Poll $poll) {
+        $this->poll = $poll;
+    }
+
+    public function hasVoted(): bool {
+        $key = $this->getVoteKey();
+        return isset($_SESSION[$key]);
+    }
+
+    public function recordVote(int $optionIndex): bool {
+        if ($this->hasVoted()) return false;
+        if ($this->poll->vote($optionIndex)) {
+            $_SESSION[$this->getVoteKey()] = true;
+            return true;
+        }
+        return false;
+    }
+
+    private function getVoteKey(): string {
+        return "voted_" . $this->poll->getId();
+    }
+}
